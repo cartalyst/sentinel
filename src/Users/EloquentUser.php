d27b2a8adf0f949bc0fd43f17adfcbf5514be121
trideout <trideout@gmail.com>
2016-08-26 19:31:24 -0700

    fixed placement of code

--- src/Users/EloquentUser.php
+++ src/Users/EloquentUser.php
@@ -201,13 +201,13 @@ class EloquentUser extends Model implements RoleableInterface, PermissibleInterf
      */
     public function inRole($role)
     {
-        if($index instanceof RoleInterface)
-        {
-            $tmp = $index;
-            $index = $instance;
-            $instance = $tmp;
-        }
         $role = array_first($this->roles, function ($index, $instance) use ($role) {
+            if($index instanceof RoleInterface)
+            {
+                $tmp = $index;
+                $index = $instance;
+                $instance = $tmp;
+            }
             if ($role instanceof RoleInterface) {
                 return ($instance->getRoleId() === $role->getRoleId());
             }
